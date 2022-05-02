7bd3d026341ee24f8edbd296e02cb0f335d28565
chandsgowda <chandansuresh007@gmail.com>
2022-05-02 19:47:39 +0530

    Initial commit

---
+++ android/app/src/main/java/com/chandansgowda/csgshop/csgshop/MainActivity.java
@@ -0,0 +1,6 @@
+package com.chandansgowda.csgshop.csgshop;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CSG Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CSG Shop'),
+      ),
+      body: Center(
+        child: Text('Let\'s build a shop!'),
+      ),
+    );
+  }
+}
