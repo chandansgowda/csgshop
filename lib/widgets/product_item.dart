04023e1dcfbc1fb9f1395912f3cfcd3a13b19df0
chandsgowda <chandansuresh007@gmail.com>
2022-05-09 21:26:00 +0530

    removed deprecated snackbar and added removeSingleItem method in cart provider

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -52,11 +52,12 @@ class ProductItem extends StatelessWidget {
                   color: Theme.of(context).accentColor),
               onPressed: () {
                 cart.addItem(product.id, product.price, product.title);
-                Scaffold.of(context).showSnackBar(
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                ScaffoldMessenger.of(context).showSnackBar(
                   SnackBar(
                     content: Text("Added item to cart !"),
                     duration: Duration(seconds: 2),
-                    action: SnackBarAction(label: "UNDO", onPressed: () => cart.removeItem(product.id)),
+                    action: SnackBarAction(label: "UNDO", onPressed: () => cart.removeSingleItem(product.id)),
                   ),
                 );
               },
