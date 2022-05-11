bb0927cedfcf075018828f30b86aee2aab309287
chandsgowda <chandansuresh007@gmail.com>
2022-05-11 20:23:33 +0530

    added post request to firebase realtime DB in addProduct method

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:csgshop/providers/product.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
-class Products with ChangeNotifier{
+class Products with ChangeNotifier {
   List<Product> _items = [
     Product(
       id: 'p1',
@@ -9,7 +12,7 @@ class Products with ChangeNotifier{
       description: 'A red shirt - it is pretty red!',
       price: 29.99,
       imageUrl:
-      'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
     ),
     Product(
       id: 'p2',
@@ -17,7 +20,7 @@ class Products with ChangeNotifier{
       description: 'A nice pair of trousers.',
       price: 59.99,
       imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
     ),
     Product(
       id: 'p3',
@@ -25,7 +28,7 @@ class Products with ChangeNotifier{
       description: 'Warm and cozy - exactly what you need for the winter.',
       price: 19.99,
       imageUrl:
-      'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
     ),
     Product(
       id: 'p4',
@@ -33,23 +36,29 @@ class Products with ChangeNotifier{
       description: 'Prepare any meal you want.',
       price: 49.99,
       imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
     ),
   ];
 
-  List<Product> get items{
+  List<Product> get items {
     return [..._items];
   }
 
-  List<Product> get favoriteItems{
-    return _items.where((element) => element.isFavourite==true).toList();
+  List<Product> get favoriteItems {
+    return _items.where((element) => element.isFavourite == true).toList();
   }
 
-  Product findById(String id){
+  Product findById(String id) {
     return _items.firstWhere((element) => element.id == id);
   }
 
-  void addProduct(Product newProduct){
+  void addProduct(Product newProduct) {
+    final url = Uri.parse("https://practice-66cd5-default-rtdb.firebaseio.com/products.json");
+    http.post(
+      url,
+      body: json
+          .encode({'title': newProduct.title, 'price': newProduct.price, 'description': newProduct.description, 'imageUrl': newProduct.imageUrl, }),
+    );
     _items.add(newProduct);
     notifyListeners();
   }
